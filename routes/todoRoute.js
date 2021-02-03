89727fca4ed4b013bcac06947c7c0b1b31ca9e3a
Sumi Kim <ksm2920@gmail.com>
2021-02-03 15:43:05 +0100

    sorting and pagination function were added

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -5,36 +5,62 @@ let router = express.Router();
 
 router.get('/', async(req, res) => {
     
-    const data = await Todo.find();
-    console.log(data);
-    res.render('index.ejs', {data:data, error:"empty"})
+    const sorted = +req.query.sorted || 1;
+    const page = +req.query.page || 1;
     
+    
+    const totalData = await Todo.find().countDocuments();
+    
+    const dataToShowPerPage = 2;
+    
+    const totalDataPart = Math.ceil(totalData/dataToShowPerPage)
+    
+    const dataToShow = dataToShowPerPage * page
+    
+    const data = await Todo.find().limit(dataToShow).sort({date: sorted})
+    
+    res.render('index.ejs', {data, totalData, totalDataPart, dataToShow, dataToShowPerPage, error:"empty"})   
 })
 
 router.post('/', async (req, res) => {
     await new Todo({
         task: req.body.task 
     }).save();
+    
     res.redirect('/');
+    
 })
 
 router.get("/edit/:id", async (req, res) => {
     
-    const data = await Todo.find();
+    // const data = await Todo.find();
     const id = req.params.id;
+
+    const sorted = +req.query.sorted || 1;
+    const page = +req.query.page || 1;
     
-    res.render('todo-edit.ejs', {data:data, id:id})
+    
+    const totalData = await Todo.find().countDocuments();
+    
+    const dataToShowPerPage = 2;
+    
+    const totalDataPart = Math.ceil(totalData/dataToShowPerPage)
+    
+    const dataToShow = dataToShowPerPage * page
+    
+    const data = await Todo.find().limit(dataToShow).sort({date: sorted})
+    
+    res.render('todo-edit.ejs', {data, totalData, totalDataPart, dataToShow, dataToShowPerPage, id:id})
     
 })
 
 router.post("/edit/:id", async (req,res) => {
     const id = req.params.id;
-
+    
     await Todo.findByIdAndUpdate(id, {task: req.body.task}, () => {  
         res.redirect("/")
-    })
+    })     
     
-   
 })
 
 router.get("/remove/:id", async (req, res) => {
