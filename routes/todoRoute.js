331dbb995d97b7177bdfef44cf9fabf1ba202f38
Sumi Kim <ksm2920@gmail.com>
2021-02-06 12:03:04 +0100

    pagination function is now working when user CRUD dotolist

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -7,33 +7,41 @@ router.get('/', async(req, res) => {
     
     const sorted = +req.query.sorted || 1;
     const page = +req.query.page || 1;
+    const editId = req.query.editId || -1;
+    
+    const totalItems = await Todo.find().countDocuments();
+    
+    const itemsToShowPerPage = 2;
+    
+    const maxPageNr = Math.ceil(totalItems/itemsToShowPerPage)
+    
+    const itemsToShow = itemsToShowPerPage * page
+    
+    const items = await Todo.find().limit(itemsToShow).sort({date: sorted})
     
     
-    const totalData = await Todo.find().countDocuments();
-    
-    const dataToShowPerPage = 2;
-    
-    const totalDataPart = Math.ceil(totalData/dataToShowPerPage)
-    
-    const dataToShow = dataToShowPerPage * page
-    
-    const data = await Todo.find().limit(dataToShow).sort({date: sorted})
-    
-    res.render('index.ejs', {data, totalData, totalDataPart, dataToShow, dataToShowPerPage, success: req.query.success, error:req.query.error})   
+    res.render('index.ejs', {
+        editId, 
+        items, 
+        page,
+        maxPageNr, 
+        sorted,
+        success: req.query.success, 
+        error:req.query.error})   
 })
 
 router.post('/', async (req, res) => {
     const page = +req.query.page || 1;
 
-    const dataToShowPerPage = 2;
-    const dataToShow = dataToShowPerPage * page
+    const itemsToShowPerPage = 2;
+    const itemsToShow = itemsToShowPerPage * page
     
     if(req.body.task) {
         await new Todo({
             task: req.body.task 
         }).save();
         
-        await Todo.find().limit(dataToShow)
+        await Todo.find().limit(itemsToShow)
         
         res.redirect("/?success=A task was added successfully!");
     } else {
@@ -42,39 +50,16 @@ router.post('/', async (req, res) => {
     
 })
 
-router.get("/edit/:id", async (req, res) => {
-    
-    // const data = await Todo.find();
-    const id = req.params.id;
-    
-    const sorted = +req.query.sorted || 1;
-    const page = +req.query.page || 1;
-    
-    
-    const totalData = await Todo.find().countDocuments();
-    
-    const dataToShowPerPage = 2;
-    
-    const totalDataPart = Math.ceil(totalData/dataToShowPerPage)
-    
-    const dataToShow = dataToShowPerPage * page
-    
-    const data = await Todo.find().limit(dataToShow).sort({date: sorted})
-    
-    res.render('todo-edit.ejs', {data, totalData, totalDataPart, dataToShow, dataToShowPerPage, id:id})
-    
-})
-
 router.post("/edit/:id", async (req,res) => {
     const id = req.params.id;
-    
-    const totalData = await Todo.find().countDocuments();
-    const dataToShowPerPage = 2;
-    const totalDataPart = Math.ceil(totalData/dataToShowPerPage);
+    const page = +req.query.page || 1;
+    // const totalData = await Todo.find().countDocuments();
+    // const itemsToShowPerPage = 2;
+    // const totalDataPart = Math.ceil(totalData/itemsToShowPerPage);
     
     await Todo.findByIdAndUpdate(id, {task: req.body.task}, () => {  
         Todo.find().countDocuments();
-        res.redirect("/?page=" + totalDataPart)
+        res.redirect("/?page=" + page + "&sorted=" + req.query.sorted)
     })     
     
 })
@@ -82,16 +67,10 @@ router.post("/edit/:id", async (req,res) => {
 router.get("/remove/:id", async (req, res) => {
     const id = req.params.id;
     
-    const totalData = await Todo.find().countDocuments();
-    const dataToShowPerPage = 2;
-    const totalDataPart = Math.ceil(totalData/dataToShowPerPage);
-    console.log(totalDataPart)
-    
     await Todo.findByIdAndRemove(id, {task: req.body.task}, () => {  
-        res.redirect("/?page=" + totalDataPart)
+        res.redirect("/?page=" + req.query.page + "&sorted=" + req.query.sorted)
     })
     
 })
-
 
 module.exports = router;
